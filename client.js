61ff54d3138d856604a12078f6460ee7af3f18ec
Nathan Carroll <ntc238@gmail.com>
2018-06-19 16:53:16 -0500

    initial commit

--- client.js
+++ client.js
@@ -1,3 +1,12 @@
+$(document).ready(start());
+
+function start(){
+  $('#submitButton').on('click', testFunction);
+}
+
+function testFunction(){
+  console.log('hi');
+}
 class Employee{
   constructor( name, employeeNumber, annualSalary, reviewRating ){
     this.name = name;
@@ -18,3 +27,56 @@ const employees = [ atticus, jem, scout, robert, mayella ];
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
 console.log( employees );
+
+for(let i = 0; i < employees.length; i++){
+  console.log(employeeComp(employees[i]));
+}
+
+
+function employeeComp(employee){
+  let bonusPercentage = bonusCalculator(employee);
+  let totalBonus = (bonusPercentage/100) * parseInt(employee.annualSalary);
+  let totalCompensation = Math.round(totalBonus + parseInt(employee.annualSalary));
+
+  let employeeBonus = {
+    name : employee.name,
+    bonusPercentage : bonusPercentage,
+    totalCompensation : totalCompensation,
+    totalBonus : totalBonus
+  };
+
+  return employeeBonus;
+
+}
+
+
+
+
+function bonusCalculator(employee){
+  let bonus = 0;
+
+  if(employee.employeeNumber.length === 4){
+    bonus+= 5;
+  }
+
+  if(parseInt(employee.annualSalary) > 65000){
+    bonus--;
+  }
+
+  if(employee.reviewRating <= 2){
+    bonus = 0;
+  }else if(employee.reviewRating == 3){
+    bonus+=4;
+  }else if (employee.reviewRating == 4){
+    bonus+=6;
+  }else if (employee.reviewRating == 5){
+    bonus+=10;
+  }
+
+
+  if(bonus >= 13){
+    bonus=13;
+  }
+
+  return bonus;
+}
